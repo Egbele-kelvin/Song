51bf0e5f781f09adb5312603bdc9c4c8d8caf7ae
kelvinfresh7 <kelvinfresh101@gmail.com>
2021-06-14 15:09:26 +0100

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    val songName: String = "Book of Life"
+    val artist : String = "Nimi Black"
+    val relaseYear: String = "2016"
+    val genre: String = "Gospel pop"
+    var duration: String = "3"
+    val albumName: String = "Made of God"
+    var numberOfStreams: Int = 20000
+    val isSongGlobal: Boolean = true
+    var numberOfDownload: Int = 20000
+
+    println(songName)
+    println(artist)
+    println(relaseYear)
+    println(genre)
+    println(duration)
+    println(albumName)
+    println(numberOfStreams)
+    println(isSongGlobal)
+    println(numberOfDownload)
+
+}
